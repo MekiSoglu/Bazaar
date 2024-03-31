4a4bd61cf1862b3f82d6b1556297a34dbe86fe8b
Zeus <105891543+cozmos12@users.noreply.github.com>
2024-03-31 21:19:38 +0300

    add base class implement

---
+++ src/main/java/by/zeus/demo/dao/CategoryRepository.java
@@ -0,0 +1,7 @@
+package by.zeus.demo.dao;
+
+import by.zeus.demo.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends BaseRepository<Category>{
+}
